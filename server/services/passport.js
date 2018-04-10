337f6e0c102bc661e36c450ea3b0156868d70350
Kamil Kruczek <kamil.kruczek@o2.pl>
2018-04-10 21:05:44 +0200

    call serializeUser with the user to generate the identifying piece of info

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,10 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
 passport.use(
   new GoogleStrategy({
       clientID: keys.googleClientID,
@@ -12,7 +16,22 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleID: profile.id }).save();
+      User.findOne({
+          googleID: profile.id
+        })
+        .then(existingUser => {
+          if (existingUser) {
+            // we already have this user record in db - do not register
+            done(null, existingUser);
+          } else {
+            // we do not have a user record with this google id - register a new record
+            new User({
+                googleID: profile.id
+              })
+              .save()
+              .then(user => done(null, user));
+          }
+        })
     }
   )
 );
